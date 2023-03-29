b576420bbe036392ba32b73d7c594ec6d84f7cb4
Natalya Babenko <natapervomaisk88@gmail.com>
2023-03-29 11:50:55 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+  selector: "my-app",
+  // template: "<h2>Angular has been started</h2>",
+  templateUrl: "./app.component.html",
+})
+export class AppComponent {
+  title: string = "Angular 15v";
+}
